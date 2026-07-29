a1790193792380a206c1b8ecd251bdceb83d44aa
Josh Cederstrom <joshcederstrom@gmail.com>
2026-07-29 15:01:16 -0700

    refactor: Remove license year from all *.h and *.cpp files

--- src/tHydro/tSnowPack.cpp
+++ src/tHydro/tSnowPack.cpp
@@ -2,7 +2,7 @@
  * TIN-based Real-time Integrated Basin Simulator (tRIBS)
  * Distributed Hydrologic Model
  *
- * Copyright (c) 2025. tRIBS Developers
+ * Copyright (c) tRIBS Developers
  *
  * See LICENSE file in the project root for full license information.
  ******************************************************************************/
